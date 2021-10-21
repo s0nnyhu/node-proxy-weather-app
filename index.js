c8fba265b1ad8e068bef6e3185b2a56980d11984
s0nnyhu <husonny1@gmail.com>
2021-10-21 19:27:52 +0200

    Weather app using node proxy

---
+++ index.js
@@ -0,0 +1,29 @@
+const express = require('express')
+const cors = require('cors')
+const rateLimit = require('express-rate-limit')
+require('dotenv').config()
+
+const PORT = process.env.PORT || 5000
+
+const app = express()
+
+//Rate limiting
+const limiter = rateLimit({
+    windowsMs: 10 * 60 * 1000, //10 minutes
+    max: 5
+});
+
+app.use(limiter)
+app.set('trust proxy', 1)
+
+//Set static folder
+app.use(express.static('public'))
+
+//Routes
+app.use('/api', require('./routes/index'))
+
+//Enable cors
+app.use(cors)
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`)
+})
